ab72224382d4ad24041ebfc778906e21c5c38dc2
Annabelle Brown <amb2528@nau.edu>
2021-12-15 15:33:26 -0700

    fix packages deletion

--- setup.py
+++ setup.py
@@ -7,6 +7,7 @@ setup(
     name="q2-autopepsirf",
     version=versioneer.get_version(),
     cmdclass = versioneer.get_cmdclass(),
+    packages = find_packages(),
     package_data={},
     author="Annabelle Brown",
     author_email="annabelle811@live.com",
