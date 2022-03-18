dd6cc63730cc714029219c3e3346dea81c2a197f
Clockwork-Rat <isaiahraspet@gmail.com>
2022-03-17 18:31:06 -0700

    initialize diffEnrich_deconve +tsv

--- q2_autopepsirf/actions/diffEnrich_deconv_tsv.py
+++ q2_autopepsirf/actions/diffEnrich_deconv_tsv.py
@@ -4,20 +4,25 @@ import qiime2
 from collections import defaultdict
 import csv, os
 
-from q2_pepsirf.format_types import PepsirfInfoSumOfProbesFmt, PepsirfInfoSNPNFormat, PepsirfContingencyTSVFormat, ZscoreNanFormat, EnrichedPeptideDirFmt
+from q2_pepsirf.format_types import (
+    PepsirfInfoSumOfProbesFmt, PepsirfInfoSNPNFormat,
+    PepsirfContingencyTSVFormat, ZscoreNanFormat,
+    EnrichedPeptideDirFmt, PeptideBinFormat,
+    EnrichThreshFileFormat
+    )
 
 def diffEnrich_deconv(
     ctx,
-    raw_data,
-    bins,
+    raw_data_tsv,
+    bins_tsv,
     infer_pairs_source=True,
     flexible_reps_source=False,
     s_enrich_source=False,
     user_defined_source = None,
-    negative_control=None,
+    negative_control_tsv=None,
     negative_id=None,
     negative_names=None,
-    thresh_file = None,
+    thresh_file_tsv = None,
     exact_z_thresh = None,
     exact_cs_thresh = "20",
     exact_zenrich_thresh = None,
@@ -29,9 +34,43 @@ def diffEnrich_deconv(
     raw_constraint = 300000,
     hdi = 0.95,
     pepsirf_binary = "pepsirf"
-):
+    ):
+
     diffEnrich = ctx.get_action('autopepsirf', 'diffEnrich')
-    deconv = ctx.get_action('pepsirf', 'deconv_batch')
+    deconv = ctx.get_action('pepsirf', 'deconv')
+
+    raw_data = ctx.make_artifact(
+        type = 'FeatureTable[RawCounts]',
+        view = raw_data_tsv,
+        view_type=PepsirfContingencyTSVFormat
+    )
+
+    bins = ctx.make_artifact(
+        type = 'PeptideBins',
+        view = bins_tsv,
+        view_type=PeptideBinFormat
+    )
+
+    if negative_control_tsv:
+        negative_control = ctx.make_artifact(
+            type='FeatureTable[Normed]',
+            view=negative_control_tsv,
+            view_type=PepsirfContingencyTSVFormat
+        )
+    # otherwise set negative control to none
+    else:
+        negative_control = None
+    
+    #if thresh-file provided import into artifact
+    if thresh_file_tsv:
+        thresh_file = ctx.make_artifact(
+            type='EnrichThresh',
+            view=thresh_file_tsv,
+            view_type=EnrichThreshFileFormat
+        )
+    #otherwise set thresh-file to none
+    else:
+        thresh_file = None
 
     ( col_sum, diff, diff_ratio, zscore_out, nan_out, sample_names,
     read_counts, rc_boxplot_out, enrich_dir, enrichedCountsBoxplot, 
@@ -59,5 +98,13 @@ def diffEnrich_deconv(
         pepsirf_binary = pepsirf_binary 
     )
 
-    #run through deconv here
+    #run through deconv
+
+    deconv_out = deconv(
+        #parameters here
+    )
+
+    #plot? I just run through deconv that's all I really know
     
+
+
