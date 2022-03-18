33f5d3f27687b0cb85c07a1cc85b376f4941e8ac
Clockwork-Rat <isaiahraspet@gmail.com>
2022-03-17 19:22:18 -0700

    add 'filled out' pipeline

--- q2_autopepsirf/actions/diffEnrich_deconv.py
+++ q2_autopepsirf/actions/diffEnrich_deconv.py
@@ -4,12 +4,25 @@ import qiime2
 from collections import defaultdict
 import csv, os
 
-from q2_pepsirf.format_types import PepsirfInfoSumOfProbesFmt, PepsirfInfoSNPNFormat, PepsirfContingencyTSVFormat, ZscoreNanFormat, EnrichedPeptideDirFmt
+from q2_pepsirf.format_types import (
+    PepsirfInfoSumOfProbesFmt, 
+    PepsirfInfoSNPNFormat, 
+    PepsirfContingencyTSVFormat, 
+    ZscoreNanFormat, 
+    EnrichedPeptideDirFmt,
+    PeptideIDListFmt,
+    PepsirfLinkTSVFormat,
+    PepsirfDMPFormat
+)
 
 def diffEnrich_deconv(
     ctx,
     raw_data,
     bins,
+    enriched: PeptideIDListFmt,
+    threshold: int,
+    linked: PepsirfLinkTSVFormat,
+    scoring_strategy: str = "summation",
     infer_pairs_source=True,
     flexible_reps_source=False,
     s_enrich_source=False,
@@ -28,6 +41,12 @@ def diffEnrich_deconv(
     lower_z_thresh = 5,
     raw_constraint = 300000,
     hdi = 0.95,
+    score_filtering: bool = False,
+    score_tie_threshold: float = 0.0,
+    score_overlap_threshold: float = 0.0,
+    id_name_map: PepsirfDMPFormat = None,
+    single_threaded: bool = False,
+    outfile: str = "./deconve.tsv",
     pepsirf_binary = "pepsirf"
 ):
     diffEnrich = ctx.get_action('autopepsirf', 'diffEnrich')
@@ -59,5 +78,21 @@ def diffEnrich_deconv(
         pepsirf_binary = pepsirf_binary 
     )
 
-    #run through deconv here
-    
+    (dir_out, score_per_round, map_dir, ) = deconv(
+        enriched = enriched,
+        threshold = threshold,
+        linked = linked,
+        scoring_strategy = scoring_strategy,
+        score_filtering = score_filtering,
+        score_tie_threshold = score_tie_threshold,
+        score_overlap_threshold = score_overlap_threshold,
+        id_name_map = id_name_map,
+        single_threaded = single_threaded,
+        outfile = outfile,
+        pepsirf_binary = pepsirf_binary
+    )
+
+    return ( col_sum, diff, diff_ratio, zscore_out, nan_out, sample_names,
+        read_counts, rc_boxplot_out, enrich_dir, enrichedCountsBoxplot, 
+        zscore_scatter, colsum_scatter, zenrich_out, dir_out, score_per_round, 
+        map_dir, )
